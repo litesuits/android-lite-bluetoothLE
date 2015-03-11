e783b11c142286ed5846f7fdf530085f1a3fd114
马天宇 <litesuits@gmail.com>
2015-03-11 17:47:18 +0800

    提交蓝牙测试

---
+++ sample/src/com/litesuits/bluetooth/sample/SampleActivity.java
@@ -0,0 +1,15 @@
+package com.litesuits.bluetooth.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
