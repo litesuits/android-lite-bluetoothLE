2f8a5fe8b7b3039dd2f8440e71969f174450258d
MaTianyu <imatianyu@qq.com>
2015-11-21 19:19:48 +0800

    提交0.9.0新版
    提交0.9.0新版

--- README.md
+++ README.md
@@ -1,2 +1,174 @@
 # LiteBle: Android Bluetooth Framework
 
+Extremely simple! Based on callback.
+
+Communication with BluetoothLE(BLE) device as easy as HTTP communication.
+
+##Usage
+
+1. scan device
+```java
+private static int TIME_OUT_SCAN = 10000;
+liteBluetooth.startLeScan(new PeriodScanCallback(TIME_OUT_SCAN) {
+    @Override
+    public void onScanTimeout() {
+        dialogShow(TIME_OUT_SCAN + " Millis Scan Timeout! ");
+    }
+
+    @Override
+    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+        BleLog.i(TAG, "device: " + device.getName() + "  mac: " + device.getAddress()
+                      + "  rssi: " + rssi + "  scanRecord: " + Arrays.toString(scanRecord));
+    }
+});
+```
+
+2. scan and connect 
+```java
+private static String MAC = "00:00:00:AA:AA:AA";
+liteBluetooth.scanAndConnect(MAC, false, new BleGattCallback() {
+
+    @Override
+    public void onConnectSuccess(BluetoothGatt gatt, int status) {
+        // discover services !
+        gatt.discoverServices();
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        BluetoothUtil.printServices(gatt);
+        dialogShow(MAC + " Services Discovered SUCCESS !");
+    }
+
+    @Override
+    public void onConnectFailure(BleException exception) {
+        bleExceptionHandler.handleException(exception);
+        dialogShow(MAC + " Services Discovered FAILURE !");
+    }
+});
+```
+
+
+3. write data to characteritic 
+```java
+LiteBleConnector connector = liteBluetooth.newBleConnector();
+connector.withUUIDString(UUID_SERVICE, UUID_CHAR_WRITE, null)
+         .writeCharacteristic(new byte[]{1, 2, 3}, new BleCharactCallback() {
+             @Override
+             public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                 BleLog.i(TAG, "Write Success, DATA: " + Arrays.toString(characteristic.getValue()));
+             }
+
+             @Override
+             public void onFailure(BleException exception) {
+                 BleLog.i(TAG, "Write failure: " + exception);
+                 bleExceptionHandler.handleException(exception);
+             }
+         });
+```
+
+4. write data to descriptor 
+```java
+LiteBleConnector connector = liteBluetooth.newBleConnector();
+connector.withUUIDString(UUID_SERVICE, UUID_CHAR_WRITE, UUID_DESCRIPTOR_WRITE)
+         .writeDescriptor(new byte[]{1, 2, 3}, new BleDescriptorCallback() {
+             @Override
+             public void onSuccess(BluetoothGattDescriptor descriptor) {
+                 BleLog.i(TAG, "Write Success, DATA: " + Arrays.toString(descriptor.getValue()));
+             }
+
+             @Override
+             public void onFailure(BleException exception) {
+                 BleLog.i(TAG, "Write failure: " + exception);
+                 bleExceptionHandler.handleException(exception);
+             }
+         });
+```
+
+5. read data from characteritic 
+```java
+LiteBleConnector connector = liteBluetooth.newBleConnector();
+connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, null)
+         .readCharacteristic(new BleCharactCallback() {
+             @Override
+             public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                 BleLog.i(TAG, "Read Success, DATA: " + Arrays.toString(characteristic.getValue()));
+             }
+
+             @Override
+             public void onFailure(BleException exception) {
+                 BleLog.i(TAG, "Read failure: " + exception);
+                 bleExceptionHandler.handleException(exception);
+             }
+         });
+```
+
+6. enable notification of characteristic
+```java
+LiteBleConnector connector = liteBluetooth.newBleConnector();
+connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, null)
+         .enableCharacteristicNotification(new BleCharactCallback() {
+             @Override
+             public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                 BleLog.i(TAG, "Notification characteristic Success, DATA: " + Arrays
+                         .toString(characteristic.getValue()));
+             }
+
+             @Override
+             public void onFailure(BleException exception) {
+                 BleLog.i(TAG, "Notification characteristic failure: " + exception);
+                 bleExceptionHandler.handleException(exception);
+             }
+         });
+```
+
+7. enable notification of descriptor
+```java
+LiteBleConnector connector = liteBluetooth.newBleConnector();
+connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, UUID_DESCRIPTOR_READ)
+         .enableDescriptorNotification(new BleDescriptorCallback() {
+             @Override
+             public void onSuccess(BluetoothGattDescriptor descriptor) {
+                 BleLog.i(TAG,
+                         "Notification descriptor Success, DATA: " + Arrays.toString(descriptor.getValue()));
+             }
+
+             @Override
+             public void onFailure(BleException exception) {
+                 BleLog.i(TAG, "Notification descriptor failure : " + exception);
+                 bleExceptionHandler.handleException(exception);
+             }
+         });
+```
+
+8. read RSSI of device
+```java
+liteBluetooth.newBleConnector()
+             .readRemoteRssi(new BleRssiCallback() {
+                 @Override
+                 public void onSuccess(int rssi) {
+                     BleLog.i(TAG, "Read Success, rssi: " + rssi);
+                 }
+
+                 @Override
+                 public void onFailure(BleException exception) {
+                     BleLog.i(TAG, "Read failure : " + exception);
+                     bleExceptionHandler.handleException(exception);
+                 }
+             });
+```
+
+
+##More Detail
+
+---
+Website : http://litesuits.com
+
+Email   : litesuits@qq.com
+
+QQgroup : [47357508][1] , [42960650][2]
+
+
+[1]: http://shang.qq.com/wpa/qunwpa?idkey=492d63aaffb04b23d8dc4df21f6b594008cbe1a819978659cddab2dbc397684e
+[2]: http://shang.qq.com/wpa/qunwpa?idkey=19bf15b9c85ec15c62141dd00618f725e2983803cd2b48566fa0e94964ae8370
+  
--- sample/src/com/litesuits/bluetooth/sample/SampleActivity.java
+++ sample/src/com/litesuits/bluetooth/sample/SampleActivity.java
@@ -5,11 +5,18 @@ import android.app.AlertDialog;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.os.Bundle;
 import android.widget.Toast;
-import com.litesuits.bluetooth.BluetoothHelper;
-import com.litesuits.bluetooth.LiteBleConnector;
+import com.litesuits.bluetooth.BleGattCallback;
 import com.litesuits.bluetooth.LiteBluetooth;
+import com.litesuits.bluetooth.conn.BleCharactCallback;
+import com.litesuits.bluetooth.conn.BleDescriptorCallback;
+import com.litesuits.bluetooth.conn.BleRssiCallback;
+import com.litesuits.bluetooth.conn.LiteBleConnector;
+import com.litesuits.bluetooth.exception.BleException;
+import com.litesuits.bluetooth.exception.hanlder.DefaultBleExceptionHandler;
+import com.litesuits.bluetooth.log.BleLog;
 import com.litesuits.bluetooth.scan.PeriodMacScanCallback;
 import com.litesuits.bluetooth.scan.PeriodScanCallback;
 import com.litesuits.bluetooth.utils.BluetoothUtil;
@@ -18,13 +25,35 @@ import java.util.Arrays;
 
 public class SampleActivity extends Activity {
 
+    private static final String TAG = SampleActivity.class.getSimpleName();
+
+    /**
+     * mac和服务uuid纯属测试，测试时请替换真实参数。
+     */
+    public String UUID_SERVICE = "6e400000-0000-0000-0000-000011112222";
+
+    public String UUID_CHAR_WRITE = "6e400001-0000-0000-0000-000011112222";
+    public String UUID_CHAR_READ = "6e400002-0000-0000-0000-000011112222";
+
+    public String UUID_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb";
+    public String UUID_DESCRIPTOR_WRITE = "00002902-0000-1000-8000-00805f9b34fb";
+    public String UUID_DESCRIPTOR_READ = "00002902-0000-1000-8000-00805f9b34fb";
+
+    private static int TIME_OUT_SCAN = 10000;
+    private static int TIME_OUT_OPERATION = 5000;
+    private Activity activity;
     /**
      * 蓝牙主要操作对象，建议单例。
      */
     private static LiteBluetooth liteBluetooth;
-    private static int TIME_OUT = 10000;
+    /**
+     * 默认异常处理器
+     */
+    private DefaultBleExceptionHandler bleExceptionHandler;
+    /**
+     * mac和服务uuid纯属测试，测试时请替换真实参数。
+     */
     private static String MAC = "00:00:00:AA:AA:AA";
-    private Activity activity;
 
     /**
      * Called when the activity is first created.
@@ -38,138 +67,79 @@ public class SampleActivity extends Activity {
             liteBluetooth = new LiteBluetooth(activity);
         }
         liteBluetooth.enableBluetoothIfDisabled(activity, 1);
-        //final BluetoothAdapter.LeScanCallback callback = new BluetoothAdapter.LeScanCallback() {
-        //    @Override
-        //    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-        //
-        //    }
-        //};
-        //liteBluetooth.startLeScan(callback);
-        //HandlerUtil.runOnUiThreadDelay(new Runnable() {
-        //    @Override
-        //    public void run() {
-        //        liteBluetooth.stopScan(callback);
-        //    }
-        //}, 30000);
-        //
-        //liteBluetooth.startLeScan(new PeriodScanCallback(30000) {
-        //    @Override
-        //    public void onScanTimeout() {
-        //
-        //    }
-        //
-        //    @Override
-        //    public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-        //
-        //    }
-        //});
-        //
-        //liteBluetooth.startLeScan(new PeriodMacScanCallback("00:00:00:AA:AA:AA", 30000) {
-        //    @Override
-        //    public void onDeviceFound(BluetoothDevice device, int rssi, byte[] scanRecord) {
-        //
-        //    }
-        //
-        //    @Override
-        //    public void onScanTimeout() {
-        //
-        //    }
-        //});
-        //
-        //ConnectListener connectListener = new ConnectListener() {
-        //
-        //    @Override
-        //    public void onStateChanged(ConnectState state) {
-        //
-        //    }
-        //
-        //    @Override
-        //    public void onFailed(ConnectError error) {
-        //
-        //    }
-        //
-        //    @Override
-        //    public void onServicesDiscovered(BluetoothGatt gatt) {
-        //        // 初始化BluetoothHelper
-        //        bluetoothHelper = new BluetoothHelper(bluetoothGatt);
-        //    }
-        //
-        //    @Override
-        //    public void onCharacteristicWrite(BluetoothGatt gatt,
-        //                                      BluetoothGattCharacteristic characteristic,
-        //                                      int status) {
-        //
-        //    }
-        //
-        //    @Override
-        //    public void onCharacteristicChanged(BluetoothGatt gatt,
-        //                                        BluetoothGattCharacteristic characteristic) {
-        //
-        //    }
-        //};
-        //// mac和服务uuid纯属测试，测试时请替换真实参数。
-        //liteBluetooth.connect("00:00:00:AA:AA:AA", false, connectListener);
+        bleExceptionHandler = new DefaultBleExceptionHandler(this);
     }
-
-    BluetoothHelper bluetoothHelper;
-
-    // mac和服务uuid纯属测试，测试时请替换真实参数。
-    public String UUID_SERVICE = "6e400000-0000-0000-0000-000011112222";
-    public String UUID_CHAR_SEND = "6e400001-0000-0000-0000-000011112222";
-    public String UUID_CHAR_RECEIVE = "6e400002-0000-0000-0000-000011112222";
-    public String UUID_DESCRIPTOR = "00002902-0000-1000-8000-000011112222";
-    protected BluetoothGattCharacteristic charSend;
-    protected BluetoothGattCharacteristic charReceive;
-
-    public void readDataOfDevice() {
-
-    }
-
 
     private void scanDevicesPeriod() {
-        liteBluetooth.startLeScan(new PeriodScanCallback(TIME_OUT) {
+        liteBluetooth.startLeScan(new PeriodScanCallback(TIME_OUT_SCAN) {
             @Override
             public void onScanTimeout() {
-                dialogShow(TIME_OUT + "毫秒 扫描结束");
+                dialogShow(TIME_OUT_SCAN + " Millis Scan Timeout! ");
             }
 
             @Override
             public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-
+                BleLog.i(TAG, "device: " + device.getName() + "  mac: " + device.getAddress()
+                              + "  rssi: " + rssi + "  scanRecord: " + Arrays.toString(scanRecord));
             }
         });
     }
 
     private void scanSpecifiedDevicePeriod() {
-        liteBluetooth.startLeScan(new PeriodMacScanCallback(MAC, TIME_OUT) {
+        liteBluetooth.startLeScan(new PeriodMacScanCallback(MAC, TIME_OUT_SCAN) {
 
             @Override
             public void onScanTimeout() {
-                dialogShow(TIME_OUT + "毫秒扫描结束，未发现设备");
+                dialogShow(TIME_OUT_SCAN + " Millis Scan Timeout!  Device Not Found! ");
             }
 
             @Override
             public void onDeviceFound(BluetoothDevice device, int rssi, byte[] scanRecord) {
-                dialogShow("发现设备 " + device.getName() + " MAC: " + device.getAddress()
+                dialogShow(" Device Found " + device.getName() + " MAC: " + device.getAddress()
                            + " \n RSSI: " + rssi + " records:" + Arrays.toString(scanRecord));
             }
         });
     }
 
+
     private void scanAndConnect() {
-        liteBluetooth.startLeScan(new PeriodMacScanCallback(MAC, TIME_OUT) {
+        liteBluetooth.scanAndConnect(MAC, false, new BleGattCallback() {
+
+            @Override
+            public void onConnectSuccess(BluetoothGatt gatt, int status) {
+                // discover services !
+                gatt.discoverServices();
+            }
+
+            @Override
+            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                BluetoothUtil.printServices(gatt);
+                dialogShow(MAC + " Services Discovered SUCCESS !");
+            }
+
+            @Override
+            public void onConnectFailure(BleException exception) {
+                bleExceptionHandler.handleException(exception);
+                dialogShow(MAC + " Services Discovered FAILURE !");
+            }
+        });
+    }
+
+    private void scanThenConnect() {
+        liteBluetooth.startLeScan(new PeriodMacScanCallback(MAC, TIME_OUT_SCAN) {
 
             @Override
             public void onScanTimeout() {
-                dialogShow(TIME_OUT + "毫秒扫描结束，未发现设备");
+                dialogShow(TIME_OUT_SCAN + "毫秒扫描结束，未发现设备");
             }
 
             @Override
             public void onDeviceFound(final BluetoothDevice device, int rssi, byte[] scanRecord) {
                 Toast.makeText(activity, "发现 " + device.getAddress() + " 正在连接...", Toast.LENGTH_LONG).show();
-                liteBluetooth.connect(device, false, new LiteBluetooth.LiteGattCallback() {
+                liteBluetooth.connect(device, false, new BleGattCallback() {
+
                     @Override
-                    public void onConnectSuccess(BluetoothGatt gatt, int status, int newState) {
+                    public void onConnectSuccess(BluetoothGatt gatt, int status) {
                         gatt.discoverServices();
                     }
 
@@ -179,7 +149,8 @@ public class SampleActivity extends Activity {
                     }
 
                     @Override
-                    public void onConnectFailure(BluetoothGatt gatt, int status, int newState) {
+                    public void onConnectFailure(BleException exception) {
+                        bleExceptionHandler.handleException(exception);
                         dialogShow(device.getAddress() + " 设备连接失败");
                     }
                 });
@@ -188,28 +159,126 @@ public class SampleActivity extends Activity {
         });
     }
 
-    private void writeData() {
+
+    private void writeDataToCharacteristic() {
         LiteBleConnector connector = liteBluetooth.newBleConnector();
-        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_SEND, null)
-                 .writeCharacteristic(new byte[]{1, 2, 3}, new LiteBleConnector.Callback() {
-
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_WRITE, null)
+                 .writeCharacteristic(new byte[]{1, 2, 3}, new BleCharactCallback() {
                      @Override
-                     public void onSuccess() {
-
+                     public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                         BleLog.i(TAG, "Write Success, DATA: " + Arrays.toString(characteristic.getValue()));
                      }
 
                      @Override
-                     public void onFailure() {
-
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Write failure: " + exception);
+                         bleExceptionHandler.handleException(exception);
                      }
                  });
     }
 
-    private void readData() {
+    private void writeDataToDescriptor() {
+        LiteBleConnector connector = liteBluetooth.newBleConnector();
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_WRITE, UUID_DESCRIPTOR_WRITE)
+                 .writeDescriptor(new byte[]{1, 2, 3}, new BleDescriptorCallback() {
+                     @Override
+                     public void onSuccess(BluetoothGattDescriptor descriptor) {
+                         BleLog.i(TAG, "Write Success, DATA: " + Arrays.toString(descriptor.getValue()));
+                     }
+
+                     @Override
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Write failure: " + exception);
+                         bleExceptionHandler.handleException(exception);
+                     }
+                 });
+    }
+
+    private void readDataFromCharacteristic() {
+        LiteBleConnector connector = liteBluetooth.newBleConnector();
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, null)
+                 .readCharacteristic(new BleCharactCallback() {
+                     @Override
+                     public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                         BleLog.i(TAG, "Read Success, DATA: " + Arrays.toString(characteristic.getValue()));
+                     }
+
+                     @Override
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Read failure: " + exception);
+                         bleExceptionHandler.handleException(exception);
+                     }
+                 });
+    }
+
+    private void readDataFromDescriptor() {
+        LiteBleConnector connector = liteBluetooth.newBleConnector();
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, UUID_DESCRIPTOR_READ)
+                 .readDescriptor(new BleDescriptorCallback() {
+                     @Override
+                     public void onSuccess(BluetoothGattDescriptor descriptor) {
+                         BleLog.i(TAG, "Read Success, DATA: " + Arrays.toString(descriptor.getValue()));
+                     }
+
+                     @Override
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Read failure : " + exception);
+                         bleExceptionHandler.handleException(exception);
+                     }
+                 });
+    }
+
+    private void enableNotificationOfCharacteristic() {
+        LiteBleConnector connector = liteBluetooth.newBleConnector();
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, null)
+                 .enableCharacteristicNotification(new BleCharactCallback() {
+                     @Override
+                     public void onSuccess(BluetoothGattCharacteristic characteristic) {
+                         BleLog.i(TAG, "Notification characteristic Success, DATA: " + Arrays
+                                 .toString(characteristic.getValue()));
+                     }
+
+                     @Override
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Notification characteristic failure: " + exception);
+                         bleExceptionHandler.handleException(exception);
+                     }
+                 });
+    }
+
+    private void enableNotificationOfDescriptor() {
+        LiteBleConnector connector = liteBluetooth.newBleConnector();
+        connector.withUUIDString(UUID_SERVICE, UUID_CHAR_READ, UUID_DESCRIPTOR_READ)
+                 .enableDescriptorNotification(new BleDescriptorCallback() {
+                     @Override
+                     public void onSuccess(BluetoothGattDescriptor descriptor) {
+                         BleLog.i(TAG,
+                                 "Notification descriptor Success, DATA: " + Arrays.toString(descriptor.getValue()));
+                     }
+
+                     @Override
+                     public void onFailure(BleException exception) {
+                         BleLog.i(TAG, "Notification descriptor failure : " + exception);
+                         bleExceptionHandler.handleException(exception);
+                     }
+                 });
     }
 
 
     public void readRssiOfDevice() {
+        liteBluetooth.newBleConnector()
+                     .readRemoteRssi(new BleRssiCallback() {
+                         @Override
+                         public void onSuccess(int rssi) {
+                             BleLog.i(TAG, "Read Success, rssi: " + rssi);
+                         }
+
+                         @Override
+                         public void onFailure(BleException exception) {
+                             BleLog.i(TAG, "Read failure : " + exception);
+                             bleExceptionHandler.handleException(exception);
+                         }
+                     });
     }
 
     public void dialogShow(String msg) {
